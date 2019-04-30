aea82378c58452c0471257c908bc4cae8654649d
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-04-30 20:35:55 +0900

    レスポンシブ対応

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,16 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+$(function(){
+  $(".fa-list").click(function(){
+    $("#menu").slideToggle();
+    return false;
+  });
+  $(window).resize(function(){
+    var win = $(window).width();
+    var p = 480;
+    if(win > p){
+      $("#menu").show();
+    }
+  });
+});
